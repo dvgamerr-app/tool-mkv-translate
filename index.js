94edb8375ccfd266a45f585ea7acb445802b1730
Kananek Thongkam <info.dvgamer@gmail.com>
2020-12-01 22:48:18 +0700

    fixed google translate api.

--- index.js
+++ index.js
@@ -47,20 +47,20 @@ const getTrackInfo = async file => {
 const translateText = async (text) => {
   let [ translations ] = await translate.translate(text.split('\\N'), { to: 'th' })
   translations = Array.isArray(translations) ? translations : [ translations ]
-  return translations.join('\\N')
+  return await translations.join('\\N')
 }
 
 const assReader = async file => {
   const buff = await fs.readFile(file)
-  await fs.unlink(file)
-
+  // await fs.copyFile(file, `source_${path.basename(file).replace(path.extname(file), '')}${path.extname(file)}`)
   let pos = 0
   const ass = await fs.open(path.basename(file), 'w')
+  await fs.unlink(file)
   for await (let line of buff.toString().split('\n')) {
     line = `${line}\n`
     if (/^Dialogue/ig.test(line)) {
       let [ , style, text ] = /(\d,[\d:.]+?,[\d:.]+?,.*?,.*?,.*?,.*?,.*?,.*?,)(.*)/ig.exec(line)
-      if (text) line = `${style}${translateText(text)}\n`
+      if (text) line = `Dialogue: ${style}${await translateText(text)}\n`
     }
     let data = Buffer.from(line, 'utf-8')
     await ass.write(data, 0, data.length, pos)
